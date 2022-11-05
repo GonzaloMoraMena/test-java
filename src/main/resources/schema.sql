59741083c88b8ffc7035a5ace0b338a1f7716edc
Gonzalo Mora <ext_gfmoram@falabella.cl>
2022-11-04 22:37:01 -0300

    first

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE lenguajes
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(130) NOT NULL
+);
+
+INSERT INTO lenguajes(name) values ('Javascript');
+INSERT INTO lenguajes(name) values ('Python');
+INSERT INTO lenguajes(name) values ('Java');
+
